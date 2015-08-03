ff9c619a07c3a10a8229a3ff8b4f391e17cb9299
Tommi Vatanen <tommivat@gmail.com>
2015-08-03 15:05:57 -0400

    update package documentation

--- R/microbiomics-package.r
+++ R/microbiomics-package.r
@@ -1,19 +1,23 @@
 #' Microbiomics
 #' 
 #' Implementation of some functions that may come handy in analysing microbiome data. 
+#' 
+#' 
 #' Following functions are included:
-#' 
-#' \code{\link{read_metaphlan_table}} read a MetaPhlAn 2.0 output file as an R data frame
-#' \code{\link{read_taxon_table}} read a summarize_taxa.py output file as an R data frame
-#' \code{\link{pairwise_spearman}} compute pairwise correlations between columns of two data sets
-#' \code{\link{get_significant_values}} get significant components among a matrix of measurements
-#' \code{\link{Maaslin.wrapper}} run MaAsLin (\url{https://bitbucket.org/biobakery/maaslin/}) given a 
-#' set of taxa and metadata.
-#' 
-#' @name microbiomics-package
-#' @docType package
+#' \describe{
+#'  \item{\code{\link{read_metaphlan_table}}}{read a MetaPhlAn 2.0 output file as an R data frame}
+#'  \item{\code{\link{read_taxon_table}}}{read a summarize_taxa.py output file as an R data frame}
+#'  \item{ \code{\link{pairwise_spearman}}}{compute pairwise correlations between columns of two data sets}
+#'  \item{\code{\link{get_significant_values}}}{get significant components among a matrix of measurements}
+#'  \item{\code{\link{Maaslin.wrapper}}}{run MaAsLin (\url{https://bitbucket.org/biobakery/maaslin/}) given a
+#' set of taxa and metadata}
+#' }
 #' 
 #' @import pspearman
 #' @import magrittr
 #' @import stringr
+#' 
+#' @docType package
+#' @name microbiomics-package
 NULL
+#> NULL
