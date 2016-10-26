a17cac999d32dfac449209fea2bc51c9b0b574f0
Tommi Vatanen <tommi.vatanen@euformatics.com>
2016-10-26 11:06:59 -0400

    Add a function of Jaccard Index

--- R/microbiomics-package.r
+++ R/microbiomics-package.r
@@ -5,6 +5,7 @@
 #' Following functions are included:
 #' \describe{
 #'  \item{\code{\link{read_metaphlan_table}}}{read a MetaPhlAn 2.0 output file as an R data frame}
+#'  \item{\code{\link{jaccard}}}{compute Jaccard index between given microbial profiles}
 #'  \item{\code{\link{read_taxon_table}}}{read a summarize_taxa.py output file as an R data frame}
 #'  \item{\code{\link{pairwise_spearman}}}{compute pairwise correlations between columns of two data sets}
 #'  \item{\code{\link{get_significant_values}}}{get significant components among a matrix of measurements}
